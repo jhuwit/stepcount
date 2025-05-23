d92758675f09267de1161e44fae6812a1bb5e9b9
muschellij2 <muschellij2@gmail.com>
2025-05-23 12:32:22 -0400

    new passes

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,11 +5,7 @@
 # Learn more about the roles of various files in:
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
-
-
-Sys.setenv("RETICULATE_PYTHON" = "managed")
 library(testthat)
 library(stepcount)
-
 
 testthat::test_check("stepcount")
