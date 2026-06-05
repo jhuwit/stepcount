ea8bd5825ccc7f76adc282bad770e8e90e24be99
muschellij2 <muschellij2@gmail.com>
2026-06-04 22:31:23 -0400

    added rlang

--- R/aaa_base.R
+++ R/aaa_base.R
@@ -2,6 +2,11 @@ stepcount_base = function() {
   sc = reticulate::import("stepcount")
   stepcount = try({sc$stepcount})
   if (inherits(stepcount, "try-error")) {
+    rlang::inform(
+      paste(capture.output(print(reticulate::py_last_error())), collapse = "\n"),
+      .frequency = "once",
+      .frequency_id = "stepcount_base_import_error"
+    )
     stepcount = reticulate::import("stepcount.stepcount")
   }
   stepcount
@@ -12,6 +17,11 @@ stepcount_base_noconvert = function() {
   sc = reticulate::import("stepcount", convert = FALSE)
   stepcount = try({sc$stepcount})
   if (inherits(stepcount, "try-error")) {
+    rlang::inform(
+      paste(capture.output(print(reticulate::py_last_error())), collapse = "\n"),
+      .frequency = "once",
+      .frequency_id = "stepcount_base_noconvert_import_error"
+    )
     stepcount = reticulate::import("stepcount.stepcount", convert = FALSE)
   }
   stepcount
