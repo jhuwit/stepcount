96b257e2b645eb8fa02df41f687b02508fb610e9
muschellij2 <muschellij2@gmail.com>
2026-06-04 21:25:14 -0400

    trying silent failure

--- R/aaa_base.R
+++ R/aaa_base.R
@@ -1,12 +1,18 @@
 stepcount_base = function() {
   sc = reticulate::import("stepcount")
-  stepcount = sc$stepcount
+  stepcount = try({sc$stepcount})
+  if (inherits(stepcount, "try-error")) {
+    stepcount = reticulate::import("stepcount.stepcount")
+  }
   stepcount
 }
 
 
 stepcount_base_noconvert = function() {
   sc = reticulate::import("stepcount", convert = FALSE)
-  stepcount = sc$stepcount
+  stepcount = try({sc$stepcount})
+  if (inherits(stepcount, "try-error")) {
+    stepcount = reticulate::import("stepcount.stepcount", convert = FALSE)
+  }
   stepcount
 }
